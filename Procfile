5c67b56d9af02bc203d9f166a8af8cbcededdc0c
adri <henintsoa.adri@hotmail.com>
2017-02-13 03:27:22 +0300

    Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -cp target/OuAiza-1.0-SNAPSHOT.war
+web: target/start -Dhttp.port=${PORT} ${JAVA_OPTS} -DapplyEvolutions.default=true -Ddb.default.driver=org.postgresql.Driver -Ddb.default.url=${DATABASE_URL}
--- src/main/java/dao/UtilDB.java
+++ src/main/java/dao/UtilDB.java
@@ -9,8 +9,8 @@ public class UtilDB {
 	public static Connection getConnPostgre()throws Exception{
 		try{
 			Class.forName("org.postgresql.Driver");
-			conn = (Connection) DriverManager.getConnection("jdbc:postgres://dxvwhjqpfuojqr:46a48570f16439720a4e1b1b69c55a0b18be02732d2b2ef2e98ab2eab0b99222@ec2-184-73-222-194.compute-1.amazonaws.com:5432/ddqlblafilkm8s","dxvwhjqpfuojqr", "46a48570f16439720a4e1b1b69c55a0b18be02732d2b2ef2e98ab2eab0b99222");
-//			conn = (Connection) DriverManager.getConnection("jdbc:postgresql://localhost:5432/OuAiza","postgres","adri");
+//			conn = (Connection) DriverManager.getConnection("jdbc:postgres://dxvwhjqpfuojqr:46a48570f16439720a4e1b1b69c55a0b18be02732d2b2ef2e98ab2eab0b99222@ec2-184-73-222-194.compute-1.amazonaws.com:5432/ddqlblafilkm8s","dxvwhjqpfuojqr", "46a48570f16439720a4e1b1b69c55a0b18be02732d2b2ef2e98ab2eab0b99222");
+			conn = (Connection) DriverManager.getConnection("jdbc:postgresql://localhost:5432/OuAiza","postgres","adri");
 			conn.setAutoCommit(false);
 			if(conn == null){
 				throw new Exception("Aucune connexion \u00e9tablie avec la base");
